04ff4676f0426de0ee9ca7cd658afb47d76d059a
Jianwei Mao <maojianwei2012@126.com>
2020-04-13 20:58:56 +0800

    App using APN6 service ok. while upgrade kernel, we should do make headers_install

--- tools/Demo_TCP_Client/src/Demo_TCP_Client.c
+++ tools/Demo_TCP_Client/src/Demo_TCP_Client.c
@@ -14,13 +14,13 @@ void main(void){
 	peerSockaddr->sin6_family = AF_INET6;
 	peerSockaddr->sin6_port = htons(5511);
 
-	int ptonRet = inet_pton(AF_INET6, "2001:da8:215:A::BBBB", &(peerSockaddr->sin6_addr.s6_addr));
+        //"2001:da8:215:A::BBBB"
+	int ptonRet = inet_pton(AF_INET6, "::1", &(peerSockaddr->sin6_addr.s6_addr));
 	//char buf[30] = {0};
 	//char * ntopRet = inet_ntop(AF_INET, &(peerSockaddr->sin_addr), buf, 29);
 
 
 	int connectSocket = socket(AF_INET6, SOCK_STREAM, 0);
-	int connectRet = connect(connectSocket, (struct sockaddr*)peerSockaddr, sizeof(struct sockaddr_in6));
 
 	unsigned char hbh[] = {
 			0x00, 0x01, 0x03, 0x0c,
@@ -28,8 +28,17 @@ void main(void){
 			0x00, 0x00, 0xaa, 0xaa,
 			0x00, 0x52, 0x27, 0x03,
 	};
-	int rettt = setsockopt(connectSocket, IPPROTO_IPV6, IPV6_HOPOPTS, hbh, 16);
-	printf("Mao setsockopt %d %d", rettt, errno);
+	//int ret = setsockopt(connectSocket, IPPROTO_IPV6, IPV6_HOPOPTS, hbh, 16);
+	//printf("Mao setsockopt %d %d", ret, errno);
+
+
+	unsigned int apn6_id[] = {0xcccc0810, 0x0000abcd, 0x00522703}; // 0x20150810
+	int ret = setsockopt(connectSocket, IPPROTO_IPV6, IPV6_APN6ID, apn6_id, sizeof(unsigned int)*3);
+	printf("Mao setsockopt %d %d", ret, errno);
+
+	int connectRet = connect(connectSocket, (struct sockaddr*)peerSockaddr, sizeof(struct sockaddr_in6));
+
+
 
 	char * html = "..........contact Beijing Tower on 118.5, good day!";
 	while(0 == errno) {
@@ -43,3 +52,9 @@ void main(void){
 
 	free(peerSockaddr);
 }
+
+
+
+
+
+
